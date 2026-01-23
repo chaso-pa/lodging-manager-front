dc383d13995e4f957f23383598c7067d791327ee
chaso-pa <130970949+chaso-pa@users.noreply.github.com>
2026-01-23 14:10:54 +0900

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import type { Metadata } from 'next';
+import '@mantine/core/styles.css';
+
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+
+export const metadata: Metadata = {
+  title: 'My Mantine app',
+  description: 'I have followed setup instructions carefully'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en' {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
