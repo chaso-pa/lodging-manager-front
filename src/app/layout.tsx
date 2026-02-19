96b35d0b18dbf009b9717007de1305a493dd12f0
chaso-pa <chaso.party.area@gmail.com>
2026-02-20 05:45:41 +0900

    update: apply styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from 'next';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
 
 import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core';
 import { AuthBootstrapper } from '@/features/auth/AuthBootstrapper';
